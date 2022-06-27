a440ded7a6ea89803355b24437a3f328cceaad87
cluelessno1 <shashwat1999@gmail.com>
2022-06-27 23:17:47 +0530

    Added game middleware in server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+app.use(require("./routes/game"));
  
 app.listen(port, async function () {
   console.log(`Server is running on port: ${port}`);
