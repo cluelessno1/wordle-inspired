f32fd9b2228707c1594c09d0ac702df4d42f9241
cluelessno1 <shashwat1999@gmail.com>
2022-06-30 23:11:51 +0530

    Working on the game api endpoint. Still a lot of work and debugging to do.

--- server/routes/game.js
+++ server/routes/game.js
@@ -5,59 +5,55 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /game.
 const recordRoutes = express.Router();
 
-// const queryDB = require("../dbQueries/queryDataFromDB.js");
+const queryQues = require("../dbQueries/queryQuestionsFromDB.js");
 const wordleCloneGame = require("../wordleCloneClass.js");
 
-let game = new wordleCloneGame();
-let currentBookID = "629776ca682fc21ec1495a2b";
-let currentBookHints = [
-    "https://i.imgur.com/KG6HCg0.png",
-
-    "https://i.imgur.com/wYcOiEM.png",
-
-    "https://i.imgur.com/Cwqc3XA.png",
-
-    "https://i.imgur.com/ew0BEsP.png",
-
-    "https://i.imgur.com/nueL8Jb.png"
-];
-
-
+let game = null;
+let currentQues = null;
 
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/game/new").post(async function (req, res) {
-    game.newGame(currentBookID,currentBookHints);
-    console.log(game.getCurrentBookID()); 
-    console.log(game.getCurrentPlayerHints());
-    res.json(game.getCurrentBookID());
+    game = new wordleCloneGame();
+    currentQues = await queryQues.queryQuestion("1");
+    console.log(currentQues); 
+    game.newGame(currentQues.value,currentQues.hints);
+    res.json({'game':"new"});
+    
 });
 
-recordRoutes.route("/game/restart").post(async function (req, res) {
+recordRoutes.route("/game/reset").post(async function (req, res) {
     game.resetStartNewGame();
-    console.log(game.getCurrentBookID()); 
-    console.log(game.getCurrentPlayerHints());
-    res.json("{ 'game':'restart'}");
+    res.json("{ 'game':'reset'}");
+    // Have to manually call /game/new endpoint from the UI side when doing Reset Game
+});
+
+recordRoutes.route("/game/next").post(async function (req, res) {
+    currentQues = await queryQues.queryQuestion(`${game.getCurrentQuestionNo()+1}`);
+    console.log(currentQues); 
+    game.newGame(currentQues.value[0],currentQues.hints);
+    res.json({'game':"next"});
+    
 });
 
 recordRoutes.route("/game/guess").post(async function (req, res) {
     if (game.guess(req.body.bookID)) {
         if (game.getGameStatus()) {
             game.nextQuestion(currentBookID,currentBookHints);
-            res.json("{'game':'nextQues'}");
+            res.json("{'game':'correct'}");
         } else {
             res.json("{ 'game':'over' }");
         }
     } else {
         if (game.getGameStatus()) {
-            res.json("{ 'game':'nextTry' }");
+            res.json("{ 'game':'incorrect' }");
         } else {
             res.json("{ 'game':'over' }");
         }
     }
-    console.log(game.getCurrentBookID()); 
-    console.log(game.getCurrentPlayerHints());
-    res.json("{ 'game':'restart'}");
+    // console.log(game.getCurrentBookID()); 
+    // console.log(game.getCurrentPlayerHints());
+    // res.json("{ 'game':'restart'}");
 });
 
 module.exports = recordRoutes;
